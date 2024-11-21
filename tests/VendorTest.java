fd6d325765cd3bf6df299708c1a384d2a61b4f33
Jake Berryman <dragons4me@comcast.net>
2024-11-21 10:15:28 -0600

    test case to validate that you can add money to a vendor

--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -3,10 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VendorTest {
-
+    Vendor vendor = new Vendor();
+    //test case to validate that you can add money to a vendor
     @Test
-    void addition() {
-        assertEquals(2, 1 + 1);
+    void addMoney() {
+        vendor.addMoney(50);
+        assertEquals(50, vendor.getBalance());
     }
-
 }
